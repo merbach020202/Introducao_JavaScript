5118eadc111bc460f68127c7d29e34886fcc42ac
Eduardo Merbach Brenn <44850641822@info.local>
2023-10-18 10:55:52 -0300

    18/10

--- ecmascript/exercicio1.js
+++ ecmascript/exercicio1.js
@@ -1,15 +1,28 @@
-const filmes = { 
-    genero: ["terror","Comedia","Ação"], 
-    titulo: ["Five nights at freddy","Gente grande"], 
+const filme = [
+{ 
+    genero: "terror", 
+    titulo: "Five nights at freddy", 
     estudio: "Sei lá",
     duracao: "2hrs"
- };
+ },
+ { 
+    genero: "comedia", 
+    titulo: "Gente grande", 
+    estudio: "Sei lá",
+    duracao: "1hr"
+ },
+ { 
+    genero: "acao", 
+    titulo: "Indiana Jones", 
+    estudio: "Sei lá",
+    duracao: "1:30hr"
+ }
+]
 
-const {genero, titulo, duracao} = filmes
+filme.forEach( ( {titulo, genero}, i ) => {
+    console.log(`
+    Filme${i+1}: ${titulo.toUpperCase()}
+    Gênero: ${genero.toString()}
+    `);
+})
 
-console.log(`
-Filme:
-    Genero: ${genero[1]}
-    Titulo: ${titulo[1]}
-    Duração: ${duracao}
-`);
